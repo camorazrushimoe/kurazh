fa47e6b367e7d36a8a79245da6cb3c7e4c7e6274
DESKTOP-74TJG7T\Philip <burshf@gmail.com>
2017-12-31 14:48:29 +0300

    no message

--- js/scripts.js
+++ js/scripts.js
@@ -72,7 +72,7 @@ $(document).ready(function(){
 		$(".main").find("audio").get(0).play();
 	})
 	$(".smallgame-body .main-buttons-unit").click(function(){
-		for(var i=0;i<10000;i++){
+		for(var i=0;i<100000;i++){
 			window.clearInterval(i);
 			window.clearTimeout(i);
 		}
@@ -111,7 +111,7 @@ $(document).ready(function(){
 
 	})
 	$(".biggame-body .main-buttons-unit").click(function(){
-		for(var i=0;i<10000;i++){
+		for(var i=0;i<100000;i++){
 			window.clearInterval(i);
 			window.clearTimeout(i);
 		}
@@ -209,7 +209,7 @@ $(document).ready(function(){
 		if($(this).hasClass("disabled")){
 
 		} else{
-			for(var i=0;i<10000;i++){
+			for(var i=0;i<100000;i++){
 				window.clearInterval(i);
 				window.clearTimeout(i);
 			}
@@ -280,7 +280,7 @@ $(document).ready(function(){
 	var lastSum=0;
 
 	$(".biggame-question-unit-answer-next").click(function(){
-		for(var i=0;i<10000;i++){
+		for(var i=0;i<100000;i++){
 			window.clearInterval(i);
 			window.clearTimeout(i);
 		}
@@ -509,9 +509,9 @@ $(document).ready(function(){
 		if($(this).parents(".biggame-question-unit").hasClass("compare")){
 			var curr=$(".biggame-question-wrap").slick('slickCurrentSlide');
 
-			//if($(this).parents(".biggame-question-unit").next().find("audio")){
-				//$(this).parents(".biggame-question-unit").next().find("audio").get(0).play();
-			//}
+			if($(this).parents(".biggame-question-unit").next().find("audio")){
+				$(this).parents(".biggame-question-unit").next().find("audio").get(0).play();
+			}
 
 		}
 
